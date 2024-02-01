84c0e91e81d11bb0f7d94397153b2330b1e90032
tom-Violence <roujux@gmail.com>
2024-02-01 12:07:49 +0100

    todo découpage composants

--- App.js
+++ App.js
@@ -2,26 +2,20 @@ import React, {useEffect, useState} from 'react';
 import {FlatList, Image, ImageBackground, SafeAreaView, ScrollView, StyleSheet, Text, View} from 'react-native';
 
 import * as Location from 'expo-location';
-import backImage from "./assets/day.jpg";
 
 
+const ThatDateItem = ({id, name, pic, temp,  hours}) => (
 
 
+    <View style={styles.item}>
 
-const ThatDateItem = ({id, name, pic, temp, description, hours})=> (
+            <Text style={styles.oneDay}>{name}{'  '}</Text>
+            <Text style={styles.oneDay}>{hours}:00 : {'  '}</Text>
+            <Text style={styles.oneDay}>{temp}°C</Text>
+            <Image style={styles.tinyLogoFC}
+                   source={{uri: pic}}
+            />
 
-
-
-    <View>
-<Text style={styles.forecastZone}><Text >
-        <Text >{name}{'  '}</Text>
-        <Text >{hours}:00 : {'  '}</Text>
-        <Text>{description}</Text>
-        <Text>{temp}°C</Text>
-        <Image style={styles.tinyLogoFC}
-            source={{uri : pic}}
-        />
-    </Text></Text>
     </View>
 
 )
@@ -44,47 +38,45 @@ function App() {
     const backImage = isDayTime ? require("./assets/day.jpg")
         : require("./assets/night.jpg")
 
-    const textColor = isDayTime ? "darkblue"
+    const textColor = isDayTime ? "black"
         : "antiquewhite"
 
-const backImageWhite = require("./assets/white-texture-background.jpg")
+    // const backImageWhite = require("./assets/white-texture-background.jpg")
     const API_KEY = '322270ae0aaaf6c97a4832d8c66fb1ef'
-     let latitude = loc?.coords?.latitude;
-     let longitude = loc?.coords?.longitude;
-     let API_W_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`;
+    let latitude = loc?.coords?.latitude;
+    let longitude = loc?.coords?.longitude;
+    let API_W_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`;
 
-     let API_F_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`;
+    let API_F_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`;
     let thatDate = [];
 
 
-     useEffect(() => {
-console.log("useeffect update with loc value :", loc)
-        loc != null  &&  getApiWData();
-         loc != null  &&  getApiFData();
-     }, [loc])
+    useEffect(() => {
+        console.log("useeffect update with loc value :", loc)
+        loc != null && getApiWData();
+        loc != null && getApiFData();
+    }, [loc])
 
 
+    useEffect(() => {
+        (async () => {
 
-         useEffect(() => {
-             (async () => {
+            let {status} = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                setErrorMsg('Permission to access location was denied');
+                return;
+            }
 
-                 let {status} = await Location.requestForegroundPermissionsAsync();
-                 if (status !== 'granted') {
-                     setErrorMsg('Permission to access location was denied');
-                     return;
-                 }
-
-                 let location = await Location.getCurrentPositionAsync({});
-                  setLoc(location);
-                 console.log(location)
-                 getApiWData();
-                 getApiFData()
-
+            let location = await Location.getCurrentPositionAsync({});
+            setLoc(location);
+            console.log(location)
+            getApiWData();
+            getApiFData()
 
 
-;
-             })();
-         }, [])
+            ;
+        })();
+    }, [])
 
 
     let text = 'Waiting..';
@@ -100,49 +92,49 @@ console.log("useeffect update with loc value :", loc)
         *********FETCH*********
     */
 
-function getApiWData() {
+    function getApiWData() {
 
         console.log('getApiWData')
-    console.log(API_W_URL)
+        console.log(API_W_URL)
 
-    fetch(API_W_URL)
-        .then(async response => {
-            if (!response.ok) {
-                throw new Error('pas de données weather trouvées');
-            }
+        fetch(API_W_URL)
+            .then(async response => {
+                if (!response.ok) {
+                    throw new Error('pas de données weather trouvées');
+                }
 
-            const json = await response.json();
-            console.log(json);
-            setWData(json);
-        }).catch(e=>{
-        console.log(API_W_URL);
-            console.log('erreur : ' , e);
-    })
-}
+                const json = await response.json();
+                console.log(json);
+                setWData(json);
+            }).catch(e => {
+            console.log(API_W_URL);
+            console.log('erreur : ', e);
+        })
+    }
 
-     function getApiFData() {
+    function getApiFData() {
 
-         fetch(API_F_URL)
-             .then(async response => {
-                 if (!response.ok) {
-                     throw new Error('pas de données forecast trouvées');
-                 }
+        fetch(API_F_URL)
+            .then(async response => {
+                if (!response.ok) {
+                    throw new Error('pas de données forecast trouvées');
+                }
 
-                 const json = await response.json();
-                 setFData(json);
-                 console.log(FData);
-                 console.log(" IMAGE !!!!!!!!!!!!!!!! " + DateCollection[0].pic)
+                const json = await response.json();
+                setFData(json);
+                console.log(FData);
+                console.log(" IMAGE !!!!!!!!!!!!!!!! " + DateCollection[0].pic)
 
 
-             }).catch(e=>{
+            }).catch(e => {
 
-             console.log('erreur : ' , e);
-         })
-     }
+            console.log('erreur : ', e);
+        })
+    }
 
-/*
-    ******** 3 HEURES*******
-*/
+    /*
+        ******** 3 HEURES*******
+    */
 
     useEffect(() => {
         for (let i = 0; i < 40; i++) {
@@ -150,39 +142,30 @@ function getApiWData() {
             let day = new Date(FData?.list?.[i].dt_txt).getDay()
 
             switch (day) {
-                case 1 :
-                    day = "Monday"
+                case 0 : day = "Sunday"
                     break
-                case 2 :
-                    day = "Tuesday"
+                case 1 : day = "Monday"
                     break
-                case 3 :
-                    day = "Wednesday"
+                case 2 : day = "Tuesday"
                     break
-                case 4 :
-                    day = "Thursday"
+                case 3 : day = "Wednesday"
                     break
-                case 5 :
-                    day = "Friday"
+                case 4 : day = "Thursday"
                     break
-                case 6 :
-                    day = "Saturday"
+                case 5 : day = "Friday"
                     break
-                case 7 :
-                    day = "Sunday"
+                case 6 : day = "Saturday"
                     break
-                default :
-                    day = "Soon"
+                default : day = "Soon"
                     break
             }
 
             thatDate.push({
                 id: i,
-                name: day  ?? 'LOADING',
-                hours: new Date(FData?.list?.[i].dt_txt).getHours(),
+                name: day ?? 'LOADING',
+                hours: new Date(FData?.list?.[i].dt_txt).getHours() ?? 'LOADING',
                 pic: `https://openweathermap.org/img/wn/${FData.list?.[i]?.weather?.[0].icon}@2x.png`,
                 temp: Math.floor(FData?.list?.[i].main?.temp),
-                description: FData?.list?.[i].weather?.[0]?.main?.description
             })
 
 
@@ -190,116 +173,113 @@ function getApiWData() {
 
         setDateCollection(thatDate)
 
-    },[FData])
+    }, [FData])
 
 
-     let tempCelsius = WData?.main?.temp?? "chargement température"/*Math.round(WData?.main.temp)*/
-     let name = WData?.name ?? 'Chargement...';
-     let description = WData.weather?.[0]?.description?? 'Chargement...';
-     let imageUrl = `https://openweathermap.org/img/wn/${WData.weather?.[0]?.icon}@2x.png`
+    let tempCelsius = WData?.main?.temp ?? "chargement température"/*Math.round(WData?.main.temp)*/
+    let name = WData?.name ?? 'Chargement...';
+    let description = WData.weather?.[0]?.description ?? 'Chargement...';
+    let imageUrl = `https://openweathermap.org/img/wn/${WData.weather?.[0]?.icon}@2x.png`
 
 
+    if (FData)
+        return (
+            <SafeAreaView>
 
-
-    return (
-<SafeAreaView >
-
-              <ImageBackground source={backImage} resizeMode="cover">
-                  <ScrollView stickyHeaderIndices={[0]}
-                              showsVerticalScrollIndicator={false}
-                              style={styles.scrollView}>
-{/*
+                <ImageBackground source={backImage} resizeMode="cover">
+                    <ScrollView horizontal={true}
+                                showsVerticalScrollIndicator={false}
+                                style={styles.scrollView}>
+                        {/*
               ****** UPPERFRAME ******
 */}
 
-          <View style={styles.container}>
+                        <View style={styles.container}>
 
+                            <View style={styles.upper}>
 
+                                <Text style={styles.mainText}>
+                                    <Text
+                                        style={{color: textColor}}>{name} {"\n"}{description}{"\n"}{"\n"}{tempCelsius}°C {"\n"}</Text>
+                                </Text>
 
-          <View style={styles.upper}>
+                                <Image
+                                    style={styles.tinyLogo}
+                                    source={{
+                                        uri: imageUrl,
+                                    }}
+                                />
+                            </View>
+                        </View>
+                    </ScrollView>
 
-             <Text style={styles.mainText}>
-                <Text style={{color: textColor}}>{name} {"\n"}{description}{"\n"}{"\n"}{tempCelsius}°C {"\n"}</Text>
-          </Text>
-
-             <Image
-                 style={styles.tinyLogo}
-                 source={{
-                     uri: imageUrl,
-                 }}
-             />
-          </View>
-            </View>
-
-{/*
+                    <ScrollView stickyHeaderIndices={[1]}
+                                showsVerticalScrollIndicator={false}
+                                style={styles.scrollView}>
+                        {/*
               ********SCROLLFRAME*******
 */}
-            {/*<ImageBackground source={backImageWhite} resizeMode="cover" >*/}
-                      <View style={styles.item}>
-            <Text style={styles.itemCalque}>
-
-                 <FlatList
-
-                     colorSetter={textColor}
-                     data={DateCollection}
-                     renderItem={({item}) => <ThatDateItem name={item.name} pic={item.pic} temp={item.temp} description={item.description} hours={item.hours}/>}
-                     keyExtractor={item => item.id}
-                     horizontal='true'
-                 />
+                        {/*<ImageBackground source={backImageWhite} resizeMode="cover" >*/}
+                        <View>
 
 
-             </Text></View>
-           {/*</ImageBackground>*/}
-              </ScrollView>
-              </ImageBackground>
+                                <FlatList
+                                    colorSetter={textColor}
+                                    data={DateCollection}
+                                    renderItem={({item}) => <ThatDateItem name={item.name} pic={item.pic}
+                                                                          temp={item.temp}
+                                                                          style={styles.test}
+                                                                          hours={item.hours}/>}
+                                                                          textColor={textColor}
+                                    keyExtractor={item => item.id}
+                                    horizontal={false}
+                                    contentContainerStyle={styles.containerStyle}
+                                />
 
+                            </View>
+                        {/*</ImageBackground>*/}
+                    </ScrollView>
+                </ImageBackground>
 
-</SafeAreaView>
+            </SafeAreaView>
         );
 
 }
 
 
-
 const styles = StyleSheet.create({
-
-
 
 
     container: {
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
     },
-    image: {
-flex: 1,
 
-    },
     scrollView: {
         marginHorizontal: 10,
 
     },
 
 
-    upper:{
-        display : 'flex',
-        flexDirection : 'row',
-        marginTop : 50,
-        marginBottom : 20,
+    upper: {
+        display: 'bloc',
+        flexDirection: 'row',
+        marginTop: 50,
+        marginBottom: 20,
         marginHorizontal: 10,
     },
-mainText:{
-  fontSize : 25,
-    textAlign : 'center',
-    padding : 5,
+    mainText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        padding: 5,
 
-},
+    },
 
     tinyLogo: {
         justifyContent: 'center',
-        margin : -30,
-        width: 200,
-        height: 200
+        margin: -40,
+        width: 150,
+        height: 150
     },
     tinyLogoFC: {
         alignItems: 'center',
@@ -307,39 +287,55 @@ mainText:{
         width: 50,
         height: 50,
     },
-    oneDay:{
-        fontSize: 24,
+    oneDay: {
+        fontSize: 16,
         textAlign: 'center',
-        margin: 3,
+        alignItems: 'center',
+        color:'lightgoldenrodyellow',
+        fontWeight : 'bold'
+
+
     },
-    itemCalque:{
 
-
+    containerStyle:{
+        flexGrow: 1, justifyContent: 'center',
+        flexDirection :'column'
 
     },
+
+
     item: {
         backgroundColor: 'rgba(5150, 255, 255, 0.3)',
-        width : '100%',
+        width: '100%',
         padding: 10,
-        borderRadius: 10,
-        margin: 10,
+        borderTopLeftRadius: 20,
+        borderTopRightRadius: 20,
+        borderBottomLeftRadius: 20,
+        borderBottomRightRadius: 20,
+        marginVertical: 15,
         display: 'flex',
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: "space-evenly",
 
+
+
     },
 
-    forecastZone:{
-       flexDirection: "row",
-       fontSize : 25,
-        fontWeight :'bold',
+    forecastZone: {
+
+        fontSize: 25,
+        marginVertical: 10,
+        alignItems : 'center',
+        fontWeight: 'bold',
         justifyContent: 'space-evenly',
 
 
 
     },
 
+
+
 });
 
- export default App;
+export default App;
